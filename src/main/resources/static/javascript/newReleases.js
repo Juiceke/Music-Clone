d18d0bf2d786729c01372448770423d8c3a9f617
Brett herrell <brett_herrell@yahoo.com>
2022-10-18 18:59:20 -0500

    did some work on an issue with items loading in

--- src/main/resources/static/javascript/newReleases.js
+++ src/main/resources/static/javascript/newReleases.js
@@ -1,4 +1,5 @@
 const content = document.getElementById("content");
+const mql = window.matchMedia("(max-width: 800px)")
 
 async function GetListOfNewReleases() {
 
@@ -16,42 +17,54 @@ async function CallListOfNewReleases() {
     const json = await this.GetListOfNewReleases();
     console.log(json);
     const parentDiv = document.createElement("div");
-    for (i = 0; i < json.items.length; i++) {
 
-            const childDiv = document.createElement("div");
-            const img = document.createElement("img");
-            const link = document.createElement("a");
-            const nameDiv = document.createElement("div");
-            const title = document.createElement("div");
+    let contentAmount;
 
-
-
-            parentDiv.setAttribute("class", "media-group");
-            childDiv.setAttribute("class", "artist")
-            link.setAttribute("href", "http://localhost:8080/artist/" + json.items[i].id);
-            link.setAttribute("class", "linkWrapper")
-            img.setAttribute("src", json.items[i].images[0].url);
-            img.setAttribute("class", "artistImg");
-            nameDiv.setAttribute("class", "artistInfo");
-            title.setAttribute("class", "title");
-
-            for (j = 0; j < json.items[i].artists.length; j++) {
-            nameDiv.innerHTML = json.items[i].artists[j].name;
-            }
-            //   make word have capital letter proceeded by lowercase letters
-//            const word = json[i].type.charAt(0) + json[i].type.substring(1).toLowerCase();
-//            title.innerHTML = word;
-
-
-            link.append(img);
-
-            parentDiv.appendChild(childDiv);
-            childDiv.appendChild(link);
-            childDiv.appendChild(nameDiv);
-            childDiv.appendChild(title);
-            content.appendChild(parentDiv);
-
+    console.log(mql);
+    if (mql.matches) {
+    console.log("bruh");
+    } else {
+    console.log("breh");
     }
+    console.log(contentAmount);
+    for (i = 0; i < json.items.length; i++) {
+                if (i < 8) {
+                const childDiv = document.createElement("div");
+                const img = document.createElement("img");
+                const link = document.createElement("a");
+                const nameDiv = document.createElement("div");
+                const title = document.createElement("div");
+
+                parentDiv.setAttribute("class", "media-group");
+                childDiv.setAttribute("class", "artist")
+                link.setAttribute("href", "http://localhost:8080/artist/" + json.items[i].id);
+                link.setAttribute("class", "linkWrapper")
+                img.setAttribute("src", json.items[i].images[0].url);
+                img.setAttribute("class", "artistImg");
+                nameDiv.setAttribute("class", "artistInfo");
+                title.setAttribute("class", "title");
+
+                for (j = 0; j < json.items[i].artists.length; j++) {
+                nameDiv.innerHTML = json.items[i].artists[j].name;
+                }
+                //   make word have capital letter proceeded by lowercase letters
+    //            const word = json[i].type.charAt(0) + json[i].type.substring(1).toLowerCase();
+    //            title.innerHTML = word;
+
+
+                    link.append(img);
+
+                    parentDiv.appendChild(childDiv);
+                    childDiv.appendChild(link);
+                    childDiv.appendChild(nameDiv);
+                    childDiv.appendChild(title);
+                    content.appendChild(parentDiv);
+                } else {
+                    return;
+                }
+
+       }
+
 }
 
 CallListOfNewReleases();
